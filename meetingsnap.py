757c82cf609df6003178c2d94ce59dd6de9842c2
Cheney Dong <cheneydc@gmail.com>
2022-11-26 20:55:03 +0800

    Update theme and related pictures

--- meetingsnap.py
+++ meetingsnap.py
@@ -9,7 +9,7 @@ import queue
 from imagehash import average_hash
 from PIL import ImageGrab, Image, ImageTk
 from ttkbootstrap import Style
-from tkinter import Label, Entry, filedialog, messagebox, ttk, E, W
+from tkinter import Label, Entry, filedialog, messagebox, ttk, E, W, N, S
 from time import sleep
 from urllib.request import urlopen
 
@@ -177,42 +177,51 @@ def stopCapture():
         pass
 
 meetingNameLabel = Label(root, text="会议名称:")
-meetingNameLabel.grid(row=0, column=0, padx=5, pady=5, sticky=W)
+meetingNameLabel.grid(row=0, column=0, padx=5, pady=5, sticky=W+E)
 
 meetingNameEntry = Entry(root)
 meetingNameEntry.grid(row=0, column=1, columnspan=2,  padx=5, pady=5, sticky=W+E)
 
 buttonChooseDir = ttk.Button(root, style='info.TButton', text="选择目录", command=getDirectory)
-buttonChooseDir.grid(row=1, padx=5, pady=5, sticky=W)
+buttonChooseDir.grid(row=1, padx=5, pady=5, sticky=W+E)
 
 buttonCapture = ttk.Button(root, style='info.TButton', text='开始截图', command=buttonCaptureClick)
-buttonCapture.grid(row=2,padx=5, pady=5, sticky=W)
+buttonCapture.grid(row=2,padx=5, pady=5, sticky=W+E)
 buttonStop = ttk.Button(root, style='primary.TButton', text='停    止', command=stopCapture)
-buttonStop.grid(row=2, column=1, padx=5, pady=5)
+buttonStop.grid(row=2, column=1, padx=5, pady=5, sticky = W+E)
 
-qcode = Image.open(get_resource_path("./qcode.jpg"))
-qcodeImg = ImageTk.PhotoImage(qcode.resize((120, 130)))
+try:
+    qcode_bytes = urlopen("https://meetingsnap.oss-cn-beijing.aliyuncs.com/ads/qcode.jpg", timeout=2).read()
+    qcode_data = io.BytesIO(qcode_bytes)
+    qcode = Image.open(qcode_data)
+except:
+    qcode = Image.open(get_resource_path("./qcode.jpg"))
+qcodeImg = ImageTk.PhotoImage(qcode.resize((149, 142)))
 qcodeLabel = Label(root, image=qcodeImg)
-qcodeLabel.grid(row=3, column=2, padx=5, pady=5, sticky=W+E)
+qcodeLabel.grid(row=3, column=2, sticky=W+E)
 qcode.close()
 
-howtouse = Image.open(get_resource_path("./howtouse.jpg"))
-howtouseImg = ImageTk.PhotoImage(howtouse.resize((220, 130)))
+try:
+    howtouse_bytes = urlopen("https://meetingsnap.oss-cn-beijing.aliyuncs.com/ads/howtouse.jpg", timeout=2).read()
+    howtouse_data = io.BytesIO(howtouse_bytes)
+    howtouse = Image.open(howtouse_data)
+except:
+    howtouse = Image.open(get_resource_path("./howtouse.jpg"))
+howtouseImg = ImageTk.PhotoImage(howtouse.resize((280, 150)))
 howtouseLabel = Label(root, image=howtouseImg)
-howtouseLabel.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky=W+E)
+howtouseLabel.grid(row=3, column=0, columnspan=2, sticky=W+E+N+S)
 howtouse.close()
 
-url = "https://meetingsnap.oss-cn-beijing.aliyuncs.com/ads/test.jpg"
+adurl = "https://meetingsnap.oss-cn-beijing.aliyuncs.com/ads/note.jpg"
 try:
-    image_bytes = urlopen(url, timeout=2).read()
+    image_bytes = urlopen(adurl, timeout=2).read()
     data_stream = io.BytesIO(image_bytes)
     ad = Image.open(data_stream)
-    adImg = ImageTk.PhotoImage(ad.resize((380, 100)))
-    adLabel = Label(root, image=adImg)
-    adLabel.grid(row=4, column=0, columnspan=3, padx=5, pady=5, sticky=W+E)
-    ad.close()
-    # root.geometry("%dx%d" % (root.winfo_width(), root.winfo_height()))
 except:
-    pass
+    ad = Image.open(get_resource_path("./note.jpg"))
+adImg = ImageTk.PhotoImage(ad.resize((430, 120)))
+adLabel = Label(root, image=adImg)
+adLabel.grid(row=4, column=0, columnspan=3, sticky=W+E)
+ad.close()
 
 root.mainloop()
